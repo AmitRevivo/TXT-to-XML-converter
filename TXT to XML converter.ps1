3c2fdb935888c9fb3f0efbf9d17fd57d1060b7b4
Amit Revivo <139372335+AmitRevivo@users.noreply.github.com>
2023-07-12 19:50:24 +0300

    Add files via upload

---
+++ TXT to XML converter.ps1
@@ -0,0 +1,4 @@
+﻿$Directory = Read-Host "Please enter your directory path" #ask user to input path
+cd $Directory #Change directory to the user input
+
+Dir -Recurse *.txt | rename-item -newname { [io.path]::ChangeExtension($_.name, "xml") } #Convert every TXT file in the folder to XML file
